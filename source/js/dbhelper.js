54f34d0429e1c9f3c3efb2549cb29d29b4ee79a3
JuanJavier1979 <jjmrestituto@gmail.com>
2018-06-13 18:00:54 +0200

    Update Source of Data from local file to development API server. Change AJAX request to Fetch

--- source/js/dbhelper.js
+++ source/js/dbhelper.js
@@ -8,27 +8,41 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 3000 // Change this to your server port
-    return `http://localhost:1337/restaurants`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
+  }
+
+  /**
+   * Check status of Fetch requests.
+   */
+  static checkStatus(response) {
+    if (response.status === 200) {
+      return Promise.resolve(response)
+    } else {
+      return Promise.reject(new Error(`Request has failed. Return status: ${response.statusText}`))
+    }
+  }
+
+  /**
+   * Convert response to JSON data
+   */
+  static json(response) {
+    return response.json()
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(DBHelper.DATABASE_URL)
+    .then(DBHelper.checkStatus)
+    .then(DBHelper.json)
+    .then(function(restaurants) {
+      callback(null, restaurants);
+    })
+    .catch(function (error) {
+      callback(error, null);
+    });
   }
 
   /**
